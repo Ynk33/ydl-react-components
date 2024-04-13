7fb87e6811d8922e31ba1787b898340035b2664f
Yannick Tirand <ytirand@gmail.com>
2024-04-13 15:43:49 +1200

    Try to figure out a problem with module not found...

--- dist/index.js
+++ dist/index.js
@@ -32,20 +32,6 @@ function getUID() {
 }
 
 /**
- * Custom hook to have a callback called repeatingly.
- * @param interval Time to wait before triggering the callback.
- * @param callback Callback to call after each interval.
- */
-function useRepeatingCallback(interval, callback) {
-    React.useEffect(function () {
-        var autoPlayInterval = setInterval(function () {
-            callback();
-        }, interval);
-        return function () { return clearInterval(autoPlayInterval); };
-    });
-}
-
-/**
  * Get the window dimensions.
  * @returns The current window dimensions.
  */
@@ -507,7 +493,6 @@ exports.TestComponent = TestComponent;
 exports.WordpressAPI = WordpressAPI;
 exports.getScroll = getScroll;
 exports.getUID = getUID;
-exports.useRepeatingCallback = useRepeatingCallback;
 exports.useScrollVisiblityObserver = useScrollVisiblityObserver;
 exports.useScrollingNavigation = useScrollingNavigation;
 exports.useWindowDimensions = useWindowDimensions;
--- dist/lib/hooks/index.d.ts
+++ dist/lib/hooks/index.d.ts
@@ -1,6 +1,5 @@
-import useRepeatingCallback from "./useRepeatingCallback";
 import useScrollingNavigation from "./useScrollingNavigation";
 import useScrollVisiblityObserver from "./useScrollVisiblityObserver";
 import useWindowDimensions from "./useWindowDimensions";
 import { Direction } from "./useScrollingNavigation";
-export { useRepeatingCallback, useScrollingNavigation, useScrollVisiblityObserver, useWindowDimensions, Direction };
+export { /*useRepeatingCallback, */ useScrollingNavigation, useScrollVisiblityObserver, useWindowDimensions, Direction };
