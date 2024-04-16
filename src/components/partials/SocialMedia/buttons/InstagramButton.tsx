aa3ae9a31b163543370cf625012ec3c7e63e0792
Yannick Tirand <ytirand@gmail.com>
2024-04-16 16:13:01 +1200

    Fixes unwanted recursive call in InstagramButton

--- src/components/partials/SocialMedia/buttons/InstagramButton.tsx
+++ src/components/partials/SocialMedia/buttons/InstagramButton.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
 import { SocialButtonProps } from "./_SocialButtonProps";
+import SocialButton from "./_SocialButton";
 import instaLogo from "./assets/instagram.svg";
 
-export default function SocialButton(props: SocialButtonProps) {
+export default function InstagramButton(props: SocialButtonProps) {
   return (
     <SocialButton {...props}>
       <Image src={instaLogo} alt="Instagram" width={props.size} height={props.size} />
